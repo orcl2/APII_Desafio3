2ad8251fe2b1850ec4b50f5d3e3ca341677155c9
William Daniel <wdarchenemy@gmail.com>
2021-11-05 01:03:44 -0500

    Implementa exercício 11.

--- desafio2_11.c
+++ desafio2_11.c
@@ -7,15 +7,13 @@
 //   4- Mostre o vetor
 // (Dica: Criar uma única função que calcule todas as opções desejadas. Recebe “opcao”,
 // “tamanho do vetor”, “vetor”).
+#include "./util/common.h"
 #include "./util/desafio2_11.h"
-#include <stdio.h>
-
-void doSomethingWithArray(int op, int arrayLength, int *vetor);
 
 int main(void){
     int arrayLength = 6;
-    int op = 0;
-    int vetor[6];
+    int *op = (int*) malloc(1 * sizeof(int));
+    int *vetor = createIntArray(arrayLength);;
     int num;
 
     printf("Informe os valores para o vetor de 6 posicoes!.\n");
@@ -27,40 +25,7 @@ int main(void){
         vetor[i] = num;
     }
 
-    printf("Escolha alguma das alternativas abaixo: \n");
-    printf("1 - Soma dos elementos\n");
-    printf("2 - Produto dos elementos\n");
-    printf("3 - Media dos elementos\n");
-    printf("4 - Mostre o vetor\n");
-    scanf("%d", &op);  
+    showMenu(op); 
 
     doSomethingWithArray(op, arrayLength, vetor);    
 }
-
-void doSomethingWithArray(int op, int arrayLength, int *vetor){
-    float aux;
-    switch(op){
-        case 1:
-            aux = 0;
-            for(int i = 0; i < arrayLength; i++)
-                aux = aux + vetor[i];
-            printf("Resultado da operacao = %.0f", aux);
-            break;
-        case 2:
-            aux = 1;
-            for(int i = 0; i < arrayLength; i++)
-                aux = aux * vetor[i];
-            printf("Resultado da operacao = %.0f", aux);
-            break;
-        case 3: 
-            aux = 0;
-            for(int i = 0; i < arrayLength; i++)
-                aux = (aux + vetor[i]) / (i + 1);
-            printf("Resultado da operacao = %.2f", aux);
-            break;
-        case 4:
-            for(int i = 0; i < arrayLength; i++)
-                printf("Vetor[%d] = %d\n", i, vetor[i]);
-            break;
-    }
-};
--- util/desafio2_11.h
+++ util/desafio2_11.h
@@ -8,59 +8,7 @@
 // (Dica: Criar uma única função que calcule todas as opções desejadas. Recebe “opcao”,
 // “tamanho do vetor”, “vetor”).
 
-#include <stdio.h>
+#include "desafio2_11.c"
 
-void doSomethingWithArray(int op, int arrayLength, int *vetor);
-
-int main(void){
-    int arrayLength = 6;
-    int op = 0;
-    int vetor[6];
-    int num;
-
-    printf("Informe os valores para o vetor de 6 posicoes!.\n");
-    
-    for(int i = 0; i < 6; i++)
-    {
-        printf("Informe um valor: ");
-        scanf("%d", &num);  
-        vetor[i] = num;
-    }
-
-    printf("Escolha alguma das alternativas abaixo: \n");
-    printf("1 - Soma dos elementos\n");
-    printf("2 - Produto dos elementos\n");
-    printf("3 - Media dos elementos\n");
-    printf("4 - Mostre o vetor\n");
-    scanf("%d", &op);  
-
-    doSomethingWithArray(op, arrayLength, vetor);    
-}
-
-void doSomethingWithArray(int op, int arrayLength, int *vetor){
-    float aux;
-    switch(op){
-        case 1:
-            aux = 0;
-            for(int i = 0; i < arrayLength; i++)
-                aux = aux + vetor[i];
-            printf("Resultado da operacao = %.0f", aux);
-            break;
-        case 2:
-            aux = 1;
-            for(int i = 0; i < arrayLength; i++)
-                aux = aux * vetor[i];
-            printf("Resultado da operacao = %.0f", aux);
-            break;
-        case 3: 
-            aux = 0;
-            for(int i = 0; i < arrayLength; i++)
-                aux = (aux + vetor[i]) / (i + 1);
-            printf("Resultado da operacao = %.2f", aux);
-            break;
-        case 4:
-            for(int i = 0; i < arrayLength; i++)
-                printf("Vetor[%d] = %d\n", i, vetor[i]);
-            break;
-    }
-};
+void doSomethingWithArray(int *op, int arrayLength, int *vetor);
+void showMenu(int *op);
